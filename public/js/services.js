9899eed3307da02d39f4003c67a909b9bf319f44
okamuuu <okamuuu@gmail.com>
2014-11-04 11:04:43 +0900

    delete public because I dont know how to share it in app.

--- public/js/services.js
+++
@@ -1,9 +0,0 @@
-var taskServices = angular.module('taskServices', ['ngResource']);
-
-taskServices.factory('Task', ['$resource',
-  function($resource){
-    return $resource('/api/tasks/:id', {}, {
-      update: { method:'PUT', params:{id:'@id'}},
-      remove: { method:'DELETE', params:{id:'@id'}}
-    });
-  }]);
